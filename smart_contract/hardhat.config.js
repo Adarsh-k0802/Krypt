9f0b18e9ac7e02975c80415b5827f334aeb4eeb6
Adarsh Kumar <adarshk0802@gmail.com>
2023-02-07 22:01:55 +0530

    contract

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,13 @@
+//https://eth-goerli.g.alchemy.com/v2/Blw-GIfg9vy0RSIDMxurtQ3Yka2nZOHk
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports={
+  solidity: '0.8.0',
+  networks:{
+    goerli:{
+      url:'https://eth-goerli.g.alchemy.com/v2/Blw-GIfg9vy0RSIDMxurtQ3Yka2nZOHk',
+      accounts:['64b5dfae959158a2d209ea0aaca5c36d2fdc7869257bc9a8c181129c1c4603e0'],
+    },
+  },
+};
